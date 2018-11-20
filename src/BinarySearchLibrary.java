b4f219d27462cddf838050c579d729c011ba11cc
whysample <zaw4@duke.edu>
2018-11-20 16:28:48 -0500

    sd

--- src/BinarySearchLibrary.java
+++ src/BinarySearchLibrary.java
@@ -90,10 +90,7 @@ public class BinarySearchLibrary {
 			}
 		}
 		T m2= list.get(low);
-		if (comp.compare(m2,target)!=0) {
-			return -1;
-		}
-		if (comp.compare(m2,target)==0) {
+		if(m2.equals(target)) {
 			return low;
 		}
 		return -1;
